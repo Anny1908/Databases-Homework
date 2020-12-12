db08478361c248d98d371ab4e1846c2ba5f46f87
Anny <annylianmarian@gmail.com>
2020-12-12 12:10:12 +0100

    tarea cyf_ecommerce

---
+++ week-2/mandatory/3-api/cyf-ecommerce-api/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cyf_ecommerce',
+    password: 'migracode20',
+    port: 5432
+});
+
+app.get("/customers", function(req, res) {
+    pool.query('SELECT * FROM customers', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.get("/suppliers", function(req, res) {
+    pool.query('SELECT * FROM suppliers', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.get("/products", function(req, res) {
+    pool.query('SELECT products.product_name, suppliers.supplier_name FROM products INNER JOIN suppliers ON products.supplier_id=suppliers.id', (error, result) => {
+        res.json(result.rows);
+    });
+});
+
+app.listen(8888, function() {
+    console.log("Server is listening on port 8888. Ready to accept requests!");
+});
